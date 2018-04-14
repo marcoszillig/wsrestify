2b7dabc86e48055c852ee8a297de3629a3b9882f
Marcos Zillig <marcos.zillig@justdigital.com.br>
2018-04-14 13:03:04 -0300

    add nodemon, first route configured

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 3000
 
 const server = restify.createServer()
 
+server.get('/', function(req, res) {
+  res.send({msg: 'Home'})
+})
+
 server.listen(port, function() {
   console.log(`Servidor de pé em http://localhost:${port}`)
   console.log('Pra derrubar o server: ctrl + C')
